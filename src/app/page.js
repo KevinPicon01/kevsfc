fc708f82dbab89498840513e25d40635880abfb6
Kevin Picón <42751420+KevinPicon01@users.noreply.github.com>
2025-12-03 16:54:07 -0500

    Update latest YouTube video ID in page.js
    Changed the hardcoded latestVideoId to 'QMKAkyKaT14' to reflect the most recent video for the specified channel.

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ const benefits = [
 
 export default async function Page() {
     const channelUrl = "https://www.youtube.com/channel/UCUIjrgbNZn8rmUeAEguB_kQ";
-    const latestVideoId = "LMtx5oFt8-I";
+    const latestVideoId = "QMKAkyKaT14";
     const channelId = "UCUIjrgbNZn8rmUeAEguB_kQ";
     const apiKey = process.env.YOUTUBE_API_KEY;
 
