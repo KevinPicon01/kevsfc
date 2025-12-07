1775cfc5d55f61293f8e45a69cde3df1243a9760
Kevin Picón <42751420+KevinPicon01@users.noreply.github.com>
2025-12-07 16:18:07 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ const benefits = [
 
 export default async function Page() {
     const channelUrl = "https://www.youtube.com/channel/UCUIjrgbNZn8rmUeAEguB_kQ";
-    const latestVideoId = "QMKAkyKaT14";
+    const latestVideoId = "gvbbdiHmcOk";
     const channelId = "UCUIjrgbNZn8rmUeAEguB_kQ";
     const apiKey = process.env.YOUTUBE_API_KEY;
 
