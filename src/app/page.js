b3079ab462d6ea5fa32e39b28beaf906a20a757b
Kevin Picón <42751420+KevinPicon01@users.noreply.github.com>
2026-01-12 14:02:22 -0500

    Update latest video ID in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ const benefits = [
 
 export default async function Page() {
     const channelUrl = "https://www.youtube.com/channel/UCUIjrgbNZn8rmUeAEguB_kQ";
-    const latestVideoId = "AV7DxdN0m8U";
+    const latestVideoId = "DqlYk830Yf4";
     const channelId = "UCUIjrgbNZn8rmUeAEguB_kQ";
     const apiKey = process.env.YOUTUBE_API_KEY;
     // Contador de suscriptores (ya existente)
